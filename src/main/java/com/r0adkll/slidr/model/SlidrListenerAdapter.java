43c5cdc1c73f2feb20839cb4bf5a62d5ccc3d6de
Filip <rj23@protonmail.com>
2018-03-01 17:49:34 +0100

    added src

---
+++ src/main/java/com/r0adkll/slidr/model/SlidrListenerAdapter.java
@@ -0,0 +1,10 @@
+package com.r0adkll.slidr.model;
+
+
+public class SlidrListenerAdapter implements SlidrListener{
+
+    @Override public void onSlideStateChanged(int state) {}
+    @Override public void onSlideChange(float percent) {}
+    @Override public void onSlideOpened() {}
+    @Override public void onSlideClosed() {}
+}
